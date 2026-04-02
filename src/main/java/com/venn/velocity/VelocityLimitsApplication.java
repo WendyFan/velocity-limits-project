964718685e64951fb72302a035138ed6009e4c2f
Wen Fan <wenfan@mac.lan>
2026-04-02 14:59:37 -0700

    Add main application entry point

---
+++ src/main/java/com/venn/velocity/VelocityLimitsApplication.java
@@ -0,0 +1,12 @@
+package com.venn.velocity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VelocityLimitsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VelocityLimitsApplication.class, args);
+    }
+}
